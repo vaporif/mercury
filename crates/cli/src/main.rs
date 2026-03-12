9e935849ed5bfa6c8c1980b7e63c6c7a227af9f5
Dmytro Onypko <vaporif@proton.me>
2026-03-12 01:17:07 +0000

    add color eyre

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -26,6 +26,7 @@ enum Commands {
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
+    color_eyre::install()?;
     tracing_subscriber::fmt()
         .with_env_filter(EnvFilter::from_default_env())
         .init();
