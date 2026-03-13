ceae794ee670ecd0b1e84c41f6e19fe8981558b0
Dmytro Onypko <vaporif@proton.me>
2026-03-13 13:38:21 +0000

    cargo fmt

--- crates/e2e/src/relayer.rs
+++ crates/e2e/src/relayer.rs
@@ -117,10 +117,8 @@ impl TestContext {
         let token_ab = token.clone();
         let token_ba = token.clone();
 
-        let join_ab =
-            tokio::spawn(async move { relay_ab.run_with_token(token_ab, None).await });
-        let join_ba =
-            tokio::spawn(async move { relay_ba.run_with_token(token_ba, None).await });
+        let join_ab = tokio::spawn(async move { relay_ab.run_with_token(token_ab, None).await });
+        let join_ba = tokio::spawn(async move { relay_ba.run_with_token(token_ba, None).await });
 
         Ok(RelayHandle {
             cancel: token,
